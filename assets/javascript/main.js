b6d422be9f02f4a26d1e2a8e3db360ea775f0c77
Renan Luquete Cavalcante <renan.cavalcante@mercadolivre.com>
2022-02-12 20:23:45 -0300

    fill about me and profissional experiences

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,23 @@
-$(document).ready(function () {
+$(function () {
+
+  $(window).scroll(function (event) {
+    var $nav = $(".navbar.bg-light");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $nav.toggleClass('mt-5', $(this).scrollTop() < $nav.height());
+  });
+
+  $('#my-years-old').text(getAge());
+
+  function getAge() {
+    var today = new Date();
+    var birthDate = new Date("1996-10-05");
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+  };
 
   $('.skillsCarousel').slick({
     infinite: true,
@@ -17,41 +36,33 @@ $(document).ready(function () {
     ]
   });
 
-  document.getElementById('formEmail')
-    .addEventListener('submit', function (event) {
-      event.preventDefault();
+  // document.getElementById('formEmail')
+  //   .addEventListener('submit', function (event) {
+  //     event.preventDefault();
 
-      const btn = document.getElementById('button').value = "Sending...";
+  //     const btn = document.getElementById('button').value = "Sending...";
 
-      const serviceID = 'service_';
-      const templateID = 'template_';
+  //     const serviceID = 'service_';
+  //     const templateID = 'template_';
 
-      emailjs.sendForm(serviceID, templateID, this)
-        .then(() => {
-          btn.value = btnFormValue;
-          Swal.fire({
-            title: "Success!",
-            text: "E-mail sent",
-            icon: 'success',
-            confirmButtonText: 'OK'
-          })
-        }, (err) => {
-          btn.value = btnFormValue;
-          Swal.fire({
-            title: "Error!",
-            text: "Error: " + JSON.stringify(err),
-            icon: 'error',
-            confirmButtonText: 'OK'
-          })
-        });
-    }
-    );
-});
-
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar.bg-light");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    $nav.toggleClass('mt-5', $(this).scrollTop() < $nav.height());
-  });
+  //     emailjs.sendForm(serviceID, templateID, this)
+  //       .then(() => {
+  //         btn.value = btnFormValue;
+  //         Swal.fire({
+  //           title: "Success!",
+  //           text: "E-mail sent",
+  //           icon: 'success',
+  //           confirmButtonText: 'OK'
+  //         })
+  //       }, (err) => {
+  //         btn.value = btnFormValue;
+  //         Swal.fire({
+  //           title: "Error!",
+  //           text: "Error: " + JSON.stringify(err),
+  //           icon: 'error',
+  //           confirmButtonText: 'OK'
+  //         })
+  //       });
+  //   }
+  //   );
 });
